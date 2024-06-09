0cfea747411294fd62f65371a10a3275cae1c042
Nguyen Vu Khai Tam <72638887+p-kTmm@users.noreply.github.com>
2024-06-09 23:55:45 +0700

    Update testing_main.py

--- testing_main.py
+++ testing_main.py
@@ -6,7 +6,7 @@ from shutil import copyfile
 
 from testing_simulation import Simulation
 from generator import TrafficGenerator
-from model1 import TestModel
+from model import TestModel
 from visualization import Visualization
 from utils import import_test_configuration, set_sumo, set_test_path
 
@@ -17,14 +17,10 @@ if __name__ == "__main__":
     sumo_cmd = set_sumo(config['gui'], config['sumocfg_file_name'], config['max_steps'])
     model_path, plot_path = set_test_path(config['models_path_name'], config['model_to_test'])
 
-    #model fc
-    # Model = TestModel(
-    #     input_dim=config['num_states'],
-    #     model_path=model_path
-    # )
-
-    #test = model dung cnn
-    Model = TestModel(input_dim=(4, 20, 1), model_path='D:\\AI\\DoAnCK2\\TLCS\\TLCS\\models\\model_7\\')
+    Model = TestModel(
+        input_dim=config['num_states'],
+        model_path=model_path
+    )
 
     TrafficGen = TrafficGenerator(
         config['max_steps'], 
@@ -57,5 +53,3 @@ if __name__ == "__main__":
 
     Visualization.save_data_and_plot(data=Simulation.reward_episode, filename='reward', xlabel='Action step', ylabel='Reward')
     Visualization.save_data_and_plot(data=Simulation.queue_length_episode, filename='queue', xlabel='Step', ylabel='Queue lenght (vehicles)')
-
-
