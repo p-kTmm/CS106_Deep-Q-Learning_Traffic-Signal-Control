965162fb87f7cd31ee0c797bad462d48db526c54
Nguyen Vu Khai Tam <72638887+p-kTmm@users.noreply.github.com>
2024-06-15 12:57:48 +0700

    Update training_simulation.py

--- training_simulation.py
+++ training_simulation.py
@@ -5,6 +5,7 @@ import timeit
 import sys
 import os
 from model import TrainModel_FC, TrainModel_CNN, TrainModel_DDQN
+from contextlib import contextmanager
 
 # phase codes based on environment.net.xml
 PHASE_NS_GREEN = 0  # action 0 code 00
@@ -15,6 +16,19 @@ PHASE_EW_GREEN = 4  # action 2 code 10
 PHASE_EW_YELLOW = 5
 PHASE_EWL_GREEN = 6  # action 3 code 11
 PHASE_EWL_YELLOW = 7
+
+@contextmanager
+def suppress_output():
+    with open(os.devnull, 'w') as devnull:
+        old_stdout = sys.stdout
+        old_stderr = sys.stderr
+        try:
+            sys.stdout = devnull
+            sys.stderr = devnull
+            yield
+        finally:
+            sys.stdout = old_stdout
+            sys.stderr = old_stderr
 
 
 class Simulation:
@@ -45,85 +59,78 @@ class Simulation:
         # first, generate the route file for this simulation and set up sumo
         self._TrafficGen.generate_routefile(seed=episode)
 
-        # Suppress verbose output by redirecting stdout and stderr ------------------------
-        original_stdout = sys.stdout
-        original_stderr = sys.stderr
-        sys.stdout = open(os.devnull, 'w')
-        sys.stderr = open(os.devnull, 'w')
-
-        traci.start(self._sumo_cmd)
-
-        # Restore stdout and stderr ------------------------
-        sys.stdout = original_stdout
-        sys.stderr = original_stderr
-
-        
         print("Simulating...")
-
-        # inits
-        self._step = 0
-        self._waiting_times = {}
-        self._sum_neg_reward = 0
-        self._sum_queue_length = 0
-        self._sum_waiting_time = 0
-        old_total_wait = 0
-        old_state = -1
-        old_action = -1
-
-        while self._step < self._max_steps:
-
-            # get current state of the intersection
-            current_state = self._get_state()
-
-            # calculate reward of previous action: (change in cumulative waiting time between actions)
-            # waiting time = seconds waited by a car since the spawn in the environment, cumulated for every car in incoming lanes
-            current_total_wait = self._collect_waiting_times()
-            reward = old_total_wait - current_total_wait
-
-            # saving the data into the memory
-            # if self._step != 0:
-            #     self._Memory.add_sample((old_state, old_action, reward, current_state))
-            if self._step != 0:
-              # Estimate the Q-value for the old state-action pair
-              old_q_value = self._Model.predict_one(old_state)[0][old_action]
-              # Calculate the target Q-value
-              max_future_q = np.max(self._Model.predict_one(current_state))
-              td_error = reward + self._gamma * max_future_q - old_q_value
-              # Add sample to the memory with the calculated TD error as priority
-              self._Memory.add_sample((old_state, old_action, reward, current_state), td_error)
-
-
-            
-            # choose the light phase to activate, based on the current state of the intersection
-            action = self._choose_action(current_state, epsilon)
-
-            # if the chosen phase is different from the last phase, activate the yellow phase
-            if self._step != 0 and old_action != action:
-                self._set_yellow_phase(old_action)
-                self._simulate(self._yellow_duration)
-
-            # execute the phase selected before
-            self._set_green_phase(action)
-            self._simulate(self._green_duration)
-
-            # saving variables for later & accumulate reward
-            old_state = current_state
-            old_action = action
-            old_total_wait = current_total_wait
-
-            # saving only the meaningful reward to better see if the agent is behaving correctly
-            if reward < 0:
-                self._sum_neg_reward += reward
-
+        # Restore stdout and stderr ------------------------
+        with suppress_output(): 
+            traci.start(self._sumo_cmd)
+    
+            # inits
+            self._step = 0
+            self._waiting_times = {}
+            self._sum_neg_reward = 0
+            self._sum_queue_length = 0
+            self._sum_waiting_time = 0
+            old_total_wait = 0
+            old_state = -1
+            old_action = -1
+    
+            while self._step < self._max_steps:
+    
+                # get current state of the intersection
+                current_state = self._get_state()
+    
+                # calculate reward of previous action: (change in cumulative waiting time between actions)
+                # waiting time = seconds waited by a car since the spawn in the environment, cumulated for every car in incoming lanes
+                current_total_wait = self._collect_waiting_times()
+                reward = old_total_wait - current_total_wait
+    
+                # saving the data into the memory
+                # if self._step != 0:
+                #     self._Memory.add_sample((old_state, old_action, reward, current_state))
+                if self._step != 0:
+                  # Estimate the Q-value for the old state-action pair
+                  old_q_value = self._Model.predict_one(old_state)[0][old_action]
+                  # Calculate the target Q-value
+                  max_future_q = np.max(self._Model.predict_one(current_state))
+                  td_error = reward + self._gamma * max_future_q - old_q_value
+                  # Add sample to the memory with the calculated TD error as priority
+                  self._Memory.add_sample((old_state, old_action, reward, current_state), td_error)
+    
+    
+                
+                # choose the light phase to activate, based on the current state of the intersection
+                action = self._choose_action(current_state, epsilon)
+    
+                # if the chosen phase is different from the last phase, activate the yellow phase
+                if self._step != 0 and old_action != action:
+                    self._set_yellow_phase(old_action)
+                    self._simulate(self._yellow_duration)
+    
+                # execute the phase selected before
+                self._set_green_phase(action)
+                self._simulate(self._green_duration)
+    
+                # saving variables for later & accumulate reward
+                old_state = current_state
+                old_action = action
+                old_total_wait = current_total_wait
+    
+                # saving only the meaningful reward to better see if the agent is behaving correctly
+                if reward < 0:
+                    self._sum_neg_reward += reward
+        # ------------------------
+        
         self._save_episode_stats()
         print("Total reward:", self._sum_neg_reward, "- Epsilon:", round(epsilon, 2))
         traci.close()
         simulation_time = round(timeit.default_timer() - start_time, 1)
 
         print("Training...")
-        start_time = timeit.default_timer()
-        for _ in range(self._training_epochs):
-            self._replay()
+        # Restore stdout and stderr ------------------------
+        with suppress_output(): 
+            start_time = timeit.default_timer()
+            for _ in range(self._training_epochs):
+                self._replay()
         training_time = round(timeit.default_timer() - start_time, 1)
 
         return simulation_time, training_time
