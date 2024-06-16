f0cf2e8fd5ef685b6a70e58ff60ca6aaaadcbc52
Nguyen Vu Khai Tam <72638887+p-kTmm@users.noreply.github.com>
2024-06-16 14:07:18 +0700

    Update model.py

--- model.py
+++ model.py
@@ -193,7 +193,7 @@ class TrainModel_DDQN:
         self._batch_size = batch_size
         self._learning_rate = learning_rate
         self.train_counter = 0
-        self.update_freq = 50
+        self.update_freq = 10
         # Main model
         self._model = self._build_model(num_layers, width)
         # Target model
